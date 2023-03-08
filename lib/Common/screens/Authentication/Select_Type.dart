bf5518bf0ada07b0f963db8da93bd26192b0469e
shady <shady.work17@gmail.com>
2023-03-08 12:28:09 +0200

    next step

--- lib/Common/screens/Authentication/Select_Type.dart
+++ lib/Common/screens/Authentication/Select_Type.dart
@@ -1,134 +1,227 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:nb_utils/nb_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:huma_life/Common/Util/constant.dart';
-import 'package:huma_life/Common/screens/Authentication/Sign_In.dart';
-import 'package:huma_life/Data/provider/connection_string_provider.dart';
+
+import '../../../Data/provider/connection_string_provider.dart';
+import '../../Util/constant.dart';
+import 'Sign_In.dart';
 
 class SelectType extends StatefulWidget {
   const SelectType({Key? key}) : super(key: key);
 
   @override
-  _SelectTypeState createState() => _SelectTypeState();
+  _selectType createState() => _selectType();
 }
 
-class _SelectTypeState extends State<SelectType> {
-  final CodeController = TextEditingController();
-  late AnimationController controllers;
+class _selectType extends State<SelectType> {
   bool showTextVerify = false;
-
+  String codeController = "";
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-
-      child: Scaffold(
-          body: Stack(
-              children: <Widget>[
-                Container(
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/bg.png"),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              SingleChildScrollView(
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const Image(image: AssetImage("assets/images/logoinit.png")),
-                      const Image(image: AssetImage("assets/images/people.png")),
-
-                      Padding(
-                        padding: const EdgeInsets.only(left: 30, right: 30),
-                        child: Consumer<ConnectStringProvider>(
-                          builder: (context, connectStringProvider, child) {
-                            return  Column(
-                              children: [
-                                Text(
-                                  'role'.tr(),
-                                  style: kTextStyle.copyWith(
-                                      fontSize: 20.0, fontWeight: FontWeight.bold),
-                                ),
-                                SizedBox(height: 10),
-                                AppTextField(
-                                  textFieldType: TextFieldType.NAME,
-                                  controller: CodeController,
-                                  decoration: InputDecoration(
-                                    labelText: 'Company Code'.tr(),
-                                    labelStyle: kTextStyle,
-                                    hintText: 'Enter Company Code'.tr(),
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(20.0),
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(height: 10),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                    border: showTextVerify ? Border.all(color: kRedColor) : Border.all(color: kMainColor),
-                                    color: showTextVerify ? kDarkWhiteSign : kBgColor,
-                                  ),
-                                  child: ListTile(
-                                    onTap: () {
-                                      setState(() {
-                                      showTextVerify = true;
-                                     connectStringProvider.getConnectionUrl(CodeController.text).then((value) => {
-                                        if (value == true) {
-                                         Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => SignIn())),
-                                        } else {
-                                          ScaffoldMessenger.of(context)
-                                              .showSnackBar(SnackBar(
-                                            content: Text(
-                                              'Invalid company code number'.tr(),
-                                              style: TextStyle(color: Colors.white),
-                                            ),
-                                            backgroundColor: Colors.red,
-                                          )).closed.then((value) {
-                                          setState(() {
-                                            showTextVerify = false;
-                                            CodeController.text = "";
-                                              });
-                                            }),
-                                          }
-                                        });
-                                      });
-                                    },
-                                    leading: const Image(
-                                      image: AssetImage("assets/images/employee.png"),
-                                    ),
-                                    title: Text(
-                                      showTextVerify ? "Verifying . . ." : 'EmployeeLogin'.tr(),
-                                      style: kTextStyle.copyWith(fontSize: 14.0, fontWeight: FontWeight.bold),
-                                    ),
-                                    subtitle: Text(
-                                      showTextVerify ? "Your code" : 'descEm'.tr(),
-                                      style: kTextStyle.copyWith(
-                                          color: kGreyTextColor, fontSize: 12.0),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            );
-                          },
-                        ),
-                      ),
-                    ],
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xfff7f6fb),
+      body: SafeArea(
+        child: Consumer<ConnectStringProvider>(
+        builder: (context, connectStringProvider, child) {
+    return Padding(
+          padding: EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: const Icon(
+                    Icons.arrow_back,
+                    size: 32,
+                    color: Colors.black54,
                   ),
                 ),
               ),
-           ),
-         ],
-       ),
-     ),
-   );
+              const SizedBox(
+                height: 18,
+              ),
+              Container(
+                width: 200,
+                height: 200,
+                // decoration: BoxDecoration(
+                //   color: Colors.deepPurple.shade50,
+                //   shape: BoxShape.circle,
+                // ),
+                child: Image.asset(
+                  'assets/images/illustration-3.png',
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              const Text(
+                'Verification',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Enter your OTP company code",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black38,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 28,
+              ),
+              Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _textFieldOTP(first: true, last: false),
+                        _textFieldOTP(first: false, last: false),
+                        _textFieldOTP(first: false, last: false),
+                        _textFieldOTP(first: false, last: false),
+                        _textFieldOTP(first: false, last: false),
+                        _textFieldOTP(first: false, last: true),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 22,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            FocusScope.of(context).unfocus();
+                            showTextVerify = true;
+                            connectStringProvider.getConnectionUrl(codeController).then((value) => {
+                              if (value == true) {
+                                Navigator.pushReplacement(context,
+                                    MaterialPageRoute(builder: (_) => const SignIn())),
+                              } else {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(SnackBar(
+                                  content: Text(
+                                    'Invalid company code number'.tr(),
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                  backgroundColor: Colors.red,
+                                )).closed.then((value) {
+                                  setState(() {
+                                    showTextVerify = false;
+                                    codeController = "";
+                                  });
+                                }),
+                              }
+                            });
+                          });
+                        },
+                        style: ButtonStyle(
+                          foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                          backgroundColor:
+                          MaterialStateProperty.all<Color>(kMainColor),
+                          shape:
+                          MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24.0),
+                            ),
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(14.0),
+                          child: Text(
+                            'Verify',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              const Text(
+                "If you didn't have company code",
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black38,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              const Text(
+                "Please Check with your Manager",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: kMainColor,
+                ),
+                textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _textFieldOTP({required bool first, last}) {
+    return SizedBox(
+      height: 80,
+      child: AspectRatio(
+        aspectRatio: 0.6,
+        child: TextField(
+          autofocus: true,
+          onChanged: (value) {
+            if (value.length == 1 && last == false) {
+              FocusScope.of(context).nextFocus();
+            }
+            if (value.isEmpty && first == false) {
+              FocusScope.of(context).previousFocus();
+            } else {
+              setState(() {
+                codeController = value.toString();
+              });
+            }
+          },
+          showCursor: false,
+          readOnly: false,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          keyboardType: TextInputType.number,
+          maxLength: 1,
+          decoration: InputDecoration(
+            counter: const Offstage(),
+            enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(width: 2, color: Colors.black12),
+                borderRadius: BorderRadius.circular(12)),
+            focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(width: 2, color: kMainColor),
+                borderRadius: BorderRadius.circular(12)),
+          ),
+        ),
+      ),
+    );
   }
 }
